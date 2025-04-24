ac4d2e42c59182b621385ddbe530469e8f1b279f
Oludire Damilare <oludire@gmail.com>
2025-04-24 01:54:16 +0100

    ✅route groups

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -1,7 +1,7 @@
 async function Docs({ params }: { params: Promise<{ slug: string[] }> }) {
   const { slug } = await params;
 
-  //   console.log(slug);
+  console.log(slug);
 
   let content;
   if (slug?.length === 2) {
@@ -12,7 +12,9 @@ async function Docs({ params }: { params: Promise<{ slug: string[] }> }) {
     );
   } else if (slug?.length === 1) {
     content = <p> Viewing docs for feature {slug.at(0)}</p>;
-  } else content = <p>Docs home page</p>;
+  }
+  // optional catch-all segment
+  else content = <p>Docs home page</p>;
 
   return content;
 }
