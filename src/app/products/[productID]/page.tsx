6df7166ce00ad399e77fd514a5667bd35e6e7fe4
Oludire Damilare <oludire@gmail.com>
2025-04-24 14:26:27 +0100

    ✅routing metadata --dynamic metadata

--- src/app/products/[productID]/page.tsx
+++ src/app/products/[productID]/page.tsx
@@ -1,4 +1,19 @@
-async function ProductItem({ params }: { params: Promise<{ productID: string }> }) {
+import { Metadata } from 'next';
+
+type Props = {
+  params: Promise<{ productID: string }>;
+};
+
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+  const { productID } = await params;
+
+  return {
+    title: `Product ${productID}`,
+    description: `All about ${productID}`
+  };
+};
+
+async function ProductItem({ params }: Props) {
   const productID = (await params).productID;
 
   return <h1>product {productID}</h1>;
