3db2bc6c8246de90ebab3f353aa5d3e8b5a19ae3
Oludire Damilare <oludire@gmail.com>
2025-04-24 15:07:00 +0100

    ✅title metadata

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,12 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  // title: 'Blog'
+  title: {
+    absolute: 'my blog'
+  }
+};
+
 function Blog() {
   return <h1>my blog</h1>;
 }
