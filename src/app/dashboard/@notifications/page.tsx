4897f0ee0989ff88a16c14d6a55ca1ce2ecb51c7
Oludire Damilare <oludire@gmail.com>
2025-04-30 15:18:38 +0100

    ✅unmatched routes

--- src/app/dashboard/@notifications/page.tsx
+++ src/app/dashboard/@notifications/page.tsx
@@ -1,7 +1,15 @@
 import { Card } from '@/ui/card';
+import Link from 'next/link';
 
 function Notifications() {
-  return <Card>notifications</Card>;
+  return (
+    <Card>
+      <div>notifications</div>
+      <div>
+        <Link href={'/dashboard/archived'}>Archived</Link>
+      </div>
+    </Card>
+  );
 }
 
 export default Notifications;
