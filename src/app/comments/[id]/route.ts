2dbd78210e73f9a7650e01e80abc8011935bda7e
Oludire Damilare <oludire@gmail.com>
2025-05-02 22:02:48 +0100

    ✅handling DELETE requests

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -7,3 +7,29 @@ export async function GET(_request: Request, { params }: { params: Promise<{ id:
 
   return Response.json(comment);
 }
+
+export async function PATCH(request: Request, { params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+
+  const body = await request.json();
+  //   console.log(body);
+  const { text } = body;
+  // find comment
+  const comment = comments.find(comment => comment?.id === +id)!;
+
+  //   update comment text
+  comment.text = text;
+
+  return Response.json(comment);
+}
+
+export async function DELETE(request: Request, { params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+
+  const index = comments.findIndex(comment => comment.id === +id);
+  const deletedComment = comments.at(index);
+
+  comments.splice(index, 1);
+
+  return Response.json(deletedComment);
+}
