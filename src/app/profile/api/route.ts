580b7771e173d25325f24875547b764407249e1b
Oludire Damilare <oludire@gmail.com>
2025-05-05 12:57:47 +0100

    ✅headers in route handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,12 @@
-export async function GET() {
-  return new Response('profile API data');
+import { headers } from 'next/headers';
+import { NextResponse } from 'next/server';
+
+export async function GET(request: NextResponse) {
+  // const requestHeaders = new Headers(request.headers);
+  // console.log(requestHeaders.get('Authorization'));
+
+  const headerList = await headers();
+  console.log(headerList.get('Authorization'));
+
+  return new Response('<h1>profile API data</h1>', { headers: { 'Content-Type': 'text/html' } });
 }
