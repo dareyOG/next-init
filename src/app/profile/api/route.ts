cc3c0f5072b70e0c6e16feabd4826be9b934aeb6
Oludire Damilare <oludire@gmail.com>
2025-05-05 13:25:23 +0100

    ✅cookies in route-handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,4 +1,4 @@
-import { headers } from 'next/headers';
+import { cookies, headers } from 'next/headers';
 import { NextResponse } from 'next/server';
 
 export async function GET(request: NextResponse) {
@@ -8,5 +8,15 @@ export async function GET(request: NextResponse) {
   const headerList = await headers();
   console.log(headerList.get('Authorization'));
 
-  return new Response('<h1>profile API data</h1>', { headers: { 'Content-Type': 'text/html' } });
+  const theme = request.cookies.get('theme');
+  console.log(theme);
+
+  const cookie = await cookies();
+  cookie.set('resultPerPage', '20');
+  const cookieResult = cookie.get('resultPerPage');
+  console.log(cookieResult);
+
+  return new Response('<h1>profile API data</h1>', {
+    headers: { 'Content-Type': 'text/html', 'Set-Cookie': 'theme=dark' }
+  });
 }
